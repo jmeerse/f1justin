9fa22f8bc49bb63ea1d96fc7c348e5219f7d7fc4
jmeerse <jmeerse@gmail.com>
2025-11-14 17:02:37 -0500

    Initial commit

---
+++ commands.R
@@ -0,0 +1,35 @@
+install.packages("f1dataR")
+library(f1dataR)
+library(tidyverse)
+
+load_drivers(2025)
+clear_cache()
+
+py_require("pip")
+
+laps2025 <- load_laps(season = 2025) #doesn't work
+
+
+load_laps(season = "current", round = "last") #doesn't work
+
+
+ham <- load_driver_telemetry(season = "current", session = "R",driver = "HAM", laps = "all")
+
+laps_2025 <- data.frame()
+for (i in 1:20) {
+
+  df <- load_session_laps(season = "current",
+                               round = i,
+                               session = "R", 
+                               add_weather = T)
+  laps_2025 <- rbind(laps_2025, df)
+}
+
+laps_2025 <- laps_2025 %>% mutate(race_date = substr(lap_start_date, 1, 10))
+laps_2025 <- laps_2025 %>% mutate(race_date = as.Date(race_date))
+
+laps_2025 %>% count(race_date) %>% print(n = 21)
+
+
+
+
