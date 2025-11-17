3920bd1bde4cd09874561e346f1f6a922f165ee2
jmeerse <jmeerse@gmail.com>
2025-11-17 07:51:53 -0500

    nov 17

--- commands.R
+++ commands.R
@@ -29,8 +29,29 @@ for (i in 1:20) {
 laps_2025 <- laps_2025 %>% mutate(race_date = substr(lap_start_date, 1, 10))
 laps_2025 <- laps_2025 %>% mutate(race_date = as.Date(race_date))
 
-laps_2025 %>% count(race_date) %>% print(n = 21)
+laps_2025 %>% count(race_date) %>% print(n = 21) #need to get race 21
+
+ave_by_race <- laps_2025 %>% 
+  filter(compound != "nan" , compound != "None") %>% 
+  group_by(race_date, compound) %>% 
+  summarise(mean_lap = mean(lap_time, na.rm = T),
+            n = n()) %>% ungroup()
+
+ave_by_race %>% ggplot(aes(x = race_date, y = mean_lap, color = compound)) + geom_point()
+
+ave_by_driver <- laps_2025 %>% 
+  group_by(race_date, driver, compound) %>% 
+  summarise(mean_lap = mean(lap_time, na.rm = T),
+            n = n()) %>% 
+  ungroup()
+
+ave_by_driver %>% ggplot(aes(x = race_date, y = mean_lap, colour = driver)) +
+  geom_line() + facet_wrap(~compound)
 
 
-
-
+laps_2025 %>% filter(compound != "nan", compound != "None", !is.na(lap_time)) %>%  
+  ggplot(aes(x = lap_number, y = lap_time, 
+             group = driver, color = compound)) + 
+  geom_line() +
+  theme_bw() +
+  facet_wrap(~race_date)
